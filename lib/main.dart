f3106763c89b8079b54787d2266311c09328744c
Sadman <2258cse00945@manarat.ac.bd>
2026-07-17 15:38:01 +0600

    feat: Refactor app structure by moving CRUDApp and HomeScreen to separate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,6 @@
+import 'package:crud_app/app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const CRUDApp());
-}
-
-class CRUDApp extends StatelessWidget {
-  const CRUDApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
 }
