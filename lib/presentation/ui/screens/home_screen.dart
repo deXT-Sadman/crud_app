c94e4dcf750841197a4740202012307741ffc660
Sadman <2258cse00945@manarat.ac.bd>
2026-07-18 15:18:50 +0600

    feat: Update HomeScreen layout to use ListView for product display and enhance product dialog

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -26,189 +26,199 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.amberAccent,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 270,
-              width: double.infinity,
-              child: Card(
-                color: Colors.grey.shade300,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ProductInfo(
-                        productTitle: "Product Name: Asus",
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      ProductInfo(
-                        productTitle: "Product Code: Asus",
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      ProductInfo(
-                        productTitle: "IMG: Asus",
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      ProductInfo(
-                        productTitle: "Qty: Asus",
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      ProductInfo(
-                        productTitle: "Unit Price: Asus",
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      ProductInfo(
-                        productTitle: "Total Price: Asus",
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      SizedBox(height: 12),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                SizedBox(
+                  height: 270,
+                  width: double.infinity,
+                  child: Card(
+                    color: Colors.grey.shade300,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          IconButton(
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                barrierDismissible: false,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text("Update Product"),
-                                        const SizedBox(width: 12),
-                                        IconButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          icon: Icon(Icons.cancel),
+                          ProductInfo(
+                            productTitle: "Product Name: Asus",
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          ProductInfo(
+                            productTitle: "Product Code: Asus",
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          ProductInfo(
+                            productTitle: "IMG: Asus",
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          ProductInfo(
+                            productTitle: "Qty: Asus",
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          ProductInfo(
+                            productTitle: "Unit Price: Asus",
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          ProductInfo(
+                            productTitle: "Total Price: Asus",
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          SizedBox(height: 12),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    barrierDismissible: false,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text("Update Product"),
+                                            const SizedBox(width: 12),
+                                            IconButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              icon: Icon(Icons.cancel),
+                                            ),
+                                          ],
                                         ),
-                                      ],
-                                    ),
-                                    content: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        TextField(
-                                          controller: _productNameController,
-                                          decoration: InputDecoration(
-                                            hintText: "Product Name",
-                                            border: OutlineInputBorder(),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 12),
-                                        TextField(
-                                          controller: _productCodeController,
-                                          decoration: InputDecoration(
-                                            hintText: "Product Code",
-                                            border: OutlineInputBorder(),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 12),
-                                        TextField(
-                                          controller: _imgController,
-                                          decoration: InputDecoration(
-                                            hintText: "Product Image",
-                                            border: OutlineInputBorder(),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 12),
-                                        TextField(
-                                          controller: _qtyController,
-                                          decoration: InputDecoration(
-                                            hintText: "Product QTY",
-                                            border: OutlineInputBorder(),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 12),
-                                        TextField(
-                                          controller:
-                                              _productUnitPriceController,
-                                          decoration: InputDecoration(
-                                            hintText: "Product Unit Price",
-                                            border: OutlineInputBorder(),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 12),
-                                        TextField(
-                                          controller:
-                                              _productTotalPriceController,
-                                          decoration: InputDecoration(
-                                            hintText: "Product Total Price",
-                                            border: OutlineInputBorder(),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 12),
-                                        SizedBox(
-                                          width: double.infinity,
-                                          child: ElevatedButton(
-                                            onPressed: () {},
-                                            style: ElevatedButton.styleFrom(
-                                              backgroundColor:
-                                                  Colors.amberAccent,
-                                              foregroundColor: Colors.black,
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(8),
+                                        content: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            TextField(
+                                              controller:
+                                                  _productNameController,
+                                              decoration: InputDecoration(
+                                                hintText: "Product Name",
+                                                border: OutlineInputBorder(),
                                               ),
                                             ),
-                                            child: Text("Add Product"),
-                                          ),
+                                            const SizedBox(height: 12),
+                                            TextField(
+                                              controller:
+                                                  _productCodeController,
+                                              decoration: InputDecoration(
+                                                hintText: "Product Code",
+                                                border: OutlineInputBorder(),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 12),
+                                            TextField(
+                                              controller: _imgController,
+                                              decoration: InputDecoration(
+                                                hintText: "Product Image",
+                                                border: OutlineInputBorder(),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 12),
+                                            TextField(
+                                              controller: _qtyController,
+                                              decoration: InputDecoration(
+                                                hintText: "Product QTY",
+                                                border: OutlineInputBorder(),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 12),
+                                            TextField(
+                                              controller:
+                                                  _productUnitPriceController,
+                                              decoration: InputDecoration(
+                                                hintText: "Product Unit Price",
+                                                border: OutlineInputBorder(),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 12),
+                                            TextField(
+                                              controller:
+                                                  _productTotalPriceController,
+                                              decoration: InputDecoration(
+                                                hintText: "Product Total Price",
+                                                border: OutlineInputBorder(),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 12),
+                                            SizedBox(
+                                              width: double.infinity,
+                                              child: ElevatedButton(
+                                                onPressed: () {},
+                                                style: ElevatedButton.styleFrom(
+                                                  backgroundColor:
+                                                      Colors.amberAccent,
+                                                  foregroundColor: Colors.black,
+                                                  shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                          8,
+                                                        ),
+                                                  ),
+                                                ),
+                                                child: Text("Add Product"),
+                                              ),
+                                            ),
+                                          ],
                                         ),
-                                      ],
-                                    ),
+                                      );
+                                    },
                                   );
                                 },
-                              );
-                            },
-                            icon: Icon(Icons.edit),
-                          ),
-                          SizedBox(width: 5),
-                          IconButton(
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                barrierDismissible: false,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: Text("Are you sure for delete?"),
-                                    content: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: Text("No"),
+                                icon: Icon(Icons.edit),
+                              ),
+                              SizedBox(width: 5),
+                              IconButton(
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    barrierDismissible: false,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text("Are you sure for delete?"),
+                                        content: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text("No"),
+                                            ),
+                                            TextButton(
+                                              onPressed: () {},
+                                              child: Text("Yes"),
+                                            ),
+                                          ],
                                         ),
-                                        TextButton(
-                                          onPressed: () {},
-                                          child: Text("Yes"),
-                                        ),
-                                      ],
-                                    ),
+                                      );
+                                    },
                                   );
                                 },
-                              );
-                            },
-                            icon: Icon(Icons.delete),
+                                icon: Icon(Icons.delete),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ],
+              ],
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
